8659b072acfce560897c76685df0f8cb0d8d443f
turowskaja@gmail.com <turowskaja@gmail.com>
2020-10-26 22:16:58 +0300

    feat: add task count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,5 +3,16 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function countCats(backyard) {
   // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let result = 0;
+
+  for(let i=0; i<backyard.length; i++){
+
+    for(j=0; j<backyard[i].length; j++){
+      if (backyard[i][j] == '^^'){
+        result += 1;
+      }
+    }
+  }
+  return result;
 };
 
