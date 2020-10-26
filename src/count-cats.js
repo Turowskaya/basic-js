32cd48e8322f39ac65efd15838f39cda6d2bf101
turowskaja@gmail.com <turowskaja@gmail.com>
2020-10-26 22:14:36 +0300

    init: start basic-js task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(backyard) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
+
